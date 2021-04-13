47cfbfcb92229fe366af18860513ce3e1c6b2200
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-04-13 21:54:30 +0200

    add(bot): #21 add basic command interface

---
+++ app/commands/info.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func Info(s *discordgo.Session, m *discordgo.MessageCreate, prefix string) {
+	command := "info"
+	description := "Give informations about Cofy Bot"
+	response := "Hi! I'm Cofy, a Discord bot made with the Golang programming language.\nMy purpose is to help you to gain C O F F E E :coffee:"
+
+	if m.Content == fmt.Sprintf("%s %s", prefix, command) {
+		s.ChannelMessageSend(m.ChannelID, response)
+	}
+
+	if m.Content == fmt.Sprintf("%s help %s", prefix, command) {
+		s.ChannelMessageSend(m.ChannelID, description)
+
+	}
+}
