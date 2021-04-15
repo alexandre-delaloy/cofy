44b533568772549dc3832341cb8a03dec9b6c56a
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-04-15 17:50:50 +0200

    add(bot): #21 add core + services for global commands maintaining

--- app/core/commands.go
+++ app/core/commands.go
@@ -3,10 +3,11 @@ package core
 import "github.com/bwmarrin/discordgo"
 
 var Prefix string = "c!"
+
 type BaseCommand struct {
-	Name           string
-	Aliases        []string
-	Description    string
+	Name        string
+	Aliases     []string
+	Description string
 }
 type ICommand interface {
 	Execute(s *discordgo.Session, m *discordgo.MessageCreate)
