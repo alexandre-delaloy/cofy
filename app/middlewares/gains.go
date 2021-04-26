b02d076ae2d4d39b403ebe7a6a1373179070d51c
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-04-26 17:25:09 +0200

    refacto(app): #35 middlewares refacto +_comments

--- app/middlewares/gains.go
+++ app/middlewares/gains.go
@@ -9,23 +9,23 @@ import (
 	"github.com/blyndusk/cofy/app/core"
 	"github.com/blyndusk/cofy/app/helpers"
 	"github.com/bwmarrin/discordgo"
-	log "github.com/sirupsen/logrus"
 )
 
 func UpdateGains(s *discordgo.Session, m *discordgo.MessageCreate, user core.User, gainedCoins int, gainedXp int) {
+	// setup url
 	url := fmt.Sprintf("%s/users/%s", helpers.EnvVar("API_URL"), m.Author.ID)
-
+	// setup payload
 	payload, err := json.Marshal(map[string]interface{}{
 		"coins": user.Coins + gainedCoins,
 		"xp":    user.Xp + gainedXp,
 	})
-	log.Info(fmt.Sprintf("cf: +%d - xp: +%d", gainedCoins, gainedXp))
 	helpers.ExitOnError("Error while parsing json", err)
-
+	// setup client
 	client := &http.Client{}
+	// put gains
 	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(payload))
 	helpers.ExitOnError("Error while putting user", err)
-
+	// setup header
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	helpers.ExitOnError("Error while doing request", err)
--- app/middlewares/user.go
+++ app/middlewares/user.go
@@ -14,38 +14,39 @@ import (
 )
 
 func GetUser(s *discordgo.Session, userId string) core.User {
+	// setup url
 	url := fmt.Sprintf("%s/users/%s", helpers.EnvVar("API_URL"), userId)
-
+	// declare user
 	var user core.User
+	// get the user
 	resp, err := http.Get(url)
 	helpers.ExitOnError("Error while getting user", err)
 	defer resp.Body.Close()
-
 	body, err := ioutil.ReadAll(resp.Body)
 	helpers.ExitOnError("Error while reading request body", err)
-
 	json.Unmarshal(body, &user)
-
 	return user
 }
 
-func CreateUser(m *discordgo.MessageCreate) {
+func CreateUser(discordUser *discordgo.User) {
+	// setup url
 	url := fmt.Sprintf("%s/users", helpers.EnvVar("API_URL"))
-
-	userId, err := strconv.Atoi(m.Author.ID)
-	values := map[string]interface{}{
-		"discord_id": userId,
-		"name":       m.Author.Username,
+	// get the string discord id
+	discordId, _ := strconv.Atoi(discordUser.ID)
+	// setup the payload
+	payload := map[string]interface{}{
+		"discord_id": discordId,
+		"name":       discordUser.Username,
 		"coins":      0,
 		"xp":         0,
 	}
-	data, err := json.Marshal(values)
+	data, err := json.Marshal(payload)
 	helpers.ExitOnError("Error while parsing json", err)
-
+	// post the user
 	resp, err := http.Post(url, "application/json",
 		bytes.NewBuffer(data))
 	helpers.ExitOnError("Error while posting user", err)
-
+	// get user response
 	var res map[string]interface{}
 	json.NewDecoder(resp.Body).Decode(&res)
 }
