f85f8a6b49f7e451c56d49b5fe29f5645a0dac80
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-04-26 17:24:50 +0200

    refacto(app): #35 core refacto +_comments

--- app/core/commands.go
+++ app/core/commands.go
@@ -2,13 +2,17 @@ package core
 
 import "github.com/bwmarrin/discordgo"
 
+// command prefix used in all app
 var Prefix string = "c!"
 
+// the command struct, with essential informations
 type BaseCommand struct {
 	Name        string
 	Aliases     []string
 	Description string
 }
+
+// the command interface, with custom methods
 type ICommand interface {
 	Execute(s *discordgo.Session, m *discordgo.MessageCreate)
 	Help(s *discordgo.Session, m *discordgo.MessageCreate)
