7e17bbf23c409b55a649d8056e9a97e9b65f0ce5
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-05-03 16:50:06 +0200

    fix(drinks): #8 - dev command

--- app/commands/dev.go
+++ app/commands/dev.go
@@ -1,12 +1,8 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/blyndusk/cofy/app/core"
-	"github.com/blyndusk/cofy/app/middlewares"
 	"github.com/blyndusk/cofy/app/services"
-	"github.com/sirupsen/logrus"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -20,7 +16,7 @@ func DevCommandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	cmd := devCommand{
 		base: core.BaseCommand{
 			Name:        "dev",
-			Aliases:     []string{"dev", "d", "!"},
+			Aliases:     []string{"dev", "!"},
 			Description: "Developper stuff",
 		},
 		stringResponse: "API: http://localhost:3003\nADM: http://localhost:3002",
@@ -35,33 +31,8 @@ func DevCommandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 }
 
 func (cmd devCommand) Execute(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// s.ChannelMessageSend(m.ChannelID, cmd.stringResponse)
-	drinks := 	middlewares.GetDrinks(s)
-	// logrus.Info(drinks)
-	var fields []*discordgo.MessageEmbedField
-	for _, s := range drinks {
-		field := discordgo.MessageEmbedField{
-			Name: fmt.Sprintf("%s %s", s.Emoji,  s.Name),
-			Value: fmt.Sprintf(":coin: : %d - :trophy: : %d", s.Price,  s.RequiredLevel),
-		}
-		logrus.Info(field)
-		fields = append(fields, &field)
-	}
-	logrus.Info(fields)
-	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-		Color:       1752220,
-		Title:      "Drinks",
-		// Description: discordUser.Discriminator,
-		// Thumbnail:   &discordgo.MessageEmbedThumbnail{URL: discordUser.AvatarURL(""), Width: 10, Height: 10},
-		Fields: fields,
-		// Footer: &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Member since %s", user.CreatedAt.Format("01-02-2006"))},
-	})
+	s.ChannelMessageSend(m.ChannelID, cmd.stringResponse)
 
-	// for i := 0; i < 11; i++ {
-	// 	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%d - %f", i, helpers.GetXpFromLevel(i)))
-
-		
-	// }
 }
 
 func (cmd devCommand) Help(s *discordgo.Session, m *discordgo.MessageCreate) {
